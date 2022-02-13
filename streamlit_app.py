cc5fe753c2a41fe95e0709d3d44a44ee18940cbd
Sashank Makanaboyina <sashankm@buffalo.edu>
2022-02-13 13:41:47 +0530

    link fixes for project

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ st.markdown("""
         <a class="nav-link" href="#work-experience">Work Experience</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#Projects"> Projects</a>
+        <a class="nav-link" href="#projects"> Projects</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#social-media">Social Media</a>
