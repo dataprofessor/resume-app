f15a869947ed9012f7d1f700bcf96cefbecaa9c7
Sashank Makanaboyina <sashankm@buffalo.edu>
2022-02-13 23:29:47 +0530

    updated dp

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ st.markdown('## Summary', unsafe_allow_html=True)
 st.info('''
 - Experienced Educator, Researcher and Administrator with almost twenty years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
 - Strong verbal and written communication skills as demonstrated by extensive participation as invited speaker at `10` conferences as well as publishing 149 research articles.
-- Strong track record in scholarly research with H-index of `32` and total citation of 3200+.
 ''')
 
 #####################
